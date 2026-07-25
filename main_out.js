cec25c089ac2717cf5c4b177bbe8357ba763c073
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-07-25 19:04:34 +0300

    Fix syntax error: missing comma in SERVERS.ffa

--- main_out.js
+++ main_out.js
@@ -279,7 +279,7 @@ function initHelloDialogScale() {
 }
 
 const SERVERS = {
-    ffa: "ffa.agar.su:6013"
+    ffa: "ffa.agar.su:6013",
     ffa2: "ffa.agar.su",
     experimental: "ffa.agar.su", // alias → Special / ffa2
     ms: "ffa.agar.su:6002",
